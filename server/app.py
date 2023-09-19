5410545e57b88308c43f103de86a8743f4ba828a
Gaitano <gaitano.george@student.moringaschool.com>
2023-09-19 11:28:40 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import db
